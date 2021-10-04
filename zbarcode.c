86c9ee111b33207fa56bd463ec49ed9e7a5a97fa
wang <ccna8008@163.com>
2021-10-04 10:47:01 +0800

    fix zbar_version
    add patch

--- zbarcode.c
+++ zbarcode.c
@@ -827,12 +827,12 @@ PHP_MSHUTDOWN_FUNCTION(zbarcode)
 */
 PHP_MINFO_FUNCTION(zbarcode)
 {
-	unsigned int major = 0, minor = 0;
+	unsigned int major = 0, minor = 0, patch = 0;
 	char *zbar_ver = NULL;
 	unsigned long magick_version;
 
-	zbar_version(&major, &minor);
-	spprintf(&zbar_ver, 24, "%d.%d", major, minor);
+	zbar_version(&major, &minor, &patch);
+	spprintf(&zbar_ver, 24, "%d.%d.%d", major, minor,patch);
 
 	php_info_print_table_start();
 	php_info_print_table_row(2, "zbarcode module",			"enabled");
